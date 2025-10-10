2278fd729eaf79bb6845d88b5774111dd4f7a4c8
sunzhengchew <szcheww@icloud.com>
2025-10-10 14:56:48 +0100

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -24,6 +24,13 @@ const HomePage = (props) => {
     else setGenreFilter(value);
   };
 
+    const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
 
   useEffect(() => {
     fetch(
@@ -53,7 +60,7 @@ const HomePage = (props) => {
     />
 
         </Grid>
-            <MovieList movies={displayedMovies} />
+                <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
       </Grid>
     </Grid>
   );
