d5ba4736afffa5f1311650215fd0c35cac08cfb8
sunzhengchew <szcheww@icloud.com>
2025-10-11 19:59:55 +0100

    Refactor - useMovie custom hook.

--- movies/src/components/movieDetails/index.jsx
+++ movies/src/components/movieDetails/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
@@ -7,7 +7,8 @@ import StarRate from "@mui/icons-material/StarRate";
 import NavigationIcon from "@mui/icons-material/Navigation";
 import Fab from "@mui/material/Fab";
 import Typography from "@mui/material/Typography";
-
+import Drawer from "@mui/material/Drawer";
+import MovieReviews from "../movieReviews"
 
 const root = {
     display: "flex",
@@ -18,9 +19,9 @@ const root = {
     margin: 0,
 };
 const chip = { margin: 0.5 };
+const MovieDetails = ({ movie }) => {  // Don't miss this!
+const [drawerOpen, setDrawerOpen] = useState(false);
 
-const MovieDetails = ( props) => {
-  const movie = props.movie
 
   return (
     <>
@@ -57,18 +58,22 @@ const MovieDetails = ( props) => {
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
-      <Fab
+            <Fab
         color="secondary"
         variant="extended"
+        onClick={() =>setDrawerOpen(true)}
         sx={{
-            position: "fixed",
-            bottom: 2,
-            right: 2
+          position: 'fixed',
+          bottom: '1em',
+          right: '1em'
         }}
       >
         <NavigationIcon />
         Reviews
       </Fab>
+      <Drawer anchor="top" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+        <MovieReviews movie={movie} />
+      </Drawer>
       </>
   );
 };
